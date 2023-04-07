7dfd8d31083ca3a0247a5b71b277feb3a56e5d38
JhuanFigueroa <figueroatrejojuan1h@gmail.com>
2023-04-07 10:52:10 -0600

    urls actividades

--- applications/actividad/urls.py
+++ applications/actividad/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 
 app_name='actividades_app'
 urlpatterns = [
@@ -15,4 +16,4 @@ urlpatterns = [
     path('login',views.login_page ,name='login'),
     path('logout',views.logout_user,name='logout')
   
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
