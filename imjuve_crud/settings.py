a70397a2f391294094c085c80305a83ce6ce693d
JhuanFigueroa <figueroatrejojuan1h@gmail.com>
2023-04-08 17:49:07 -0600

    setings

--- imjuve_crud/settings.py
+++ imjuve_crud/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'imjuve_crud.wsgi.application'
-
+CSRF_TRUSTED_ORIGINS = ['hhttps://adminpyt-production.up.railway.app']
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
